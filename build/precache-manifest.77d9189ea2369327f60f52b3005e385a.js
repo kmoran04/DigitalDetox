267a9bf9ff09c8f9ba4ee8b9bf6d3e86a3e55972
kmoran04 <kristen.moran@tufts.edu>
2019-11-16 18:34:24 -0500

    build

---
+++ build/precache-manifest.77d9189ea2369327f60f52b3005e385a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619d284c64d0a1aebf618be99220fbc5",
+    "url": "/DigitalDetox/index.html"
+  },
+  {
+    "revision": "d4bf995b012cbfbc0d6e",
+    "url": "/DigitalDetox/static/css/main.c3843f3d.chunk.css"
+  },
+  {
+    "revision": "a8b18c8d8a4f10b17456",
+    "url": "/DigitalDetox/static/js/2.46930c7f.chunk.js"
+  },
+  {
+    "revision": "d4bf995b012cbfbc0d6e",
+    "url": "/DigitalDetox/static/js/main.86d2ec02.chunk.js"
+  },
+  {
+    "revision": "e3bf04ff4ae83d2ee8c6",
+    "url": "/DigitalDetox/static/js/runtime-main.681439cf.js"
+  },
+  {
+    "revision": "a33f8036b6dcdea310f03d2781b1fcb5",
+    "url": "/DigitalDetox/static/media/logo.a33f8036.png"
+  }
+]);
